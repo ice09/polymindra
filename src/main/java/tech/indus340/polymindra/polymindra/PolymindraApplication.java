9f0e4c5aa6edc2f88e38b6c97ef151699ee68fc4
Alexander C <alexander.culum@gmail.com>
2023-10-05 23:28:16 +0200

    Add REST interface

---
+++ src/main/java/tech/indus340/polymindra/polymindra/PolymindraApplication.java
@@ -0,0 +1,13 @@
+package tech.indus340.polymindra.polymindra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolymindraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolymindraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/indus340/polymindra/polymindra/PolymindraApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.indus340.polymindra.polymindra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolymindraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
